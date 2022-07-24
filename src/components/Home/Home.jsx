012f07a1b3ca777f374c9efafdd25d18811db44b
sameep-s <sameep.sharma98@gmail.com>
2022-07-24 13:56:12 +0530

    added:home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from 'react';
+import HeroImage from '../../assets/heroImage.png';
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+
 
 const Home = () => {
     return (
         <>
-            <div>Home</div>
+            <div name="home" className=" w-full bg-gradient-to-b from-black via-black to-gray-800 h-screen">
+
+                <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row ">
+
+                    <div className=" flex flex-col justify-center h-full ">
+
+                        <h2 className='text-4xl md:text-7xl font-bold text-white' >I'm a Front-End Developer</h2>
+                        <p className='text-gray-200 py-4 max-w-md'>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate temporibus officia ipsum, exercitationem esse at voluptas nulla asperiores ex saepe vero a? Facilis, nesciunt ad?
+                        </p>
+                        <div className="">
+                            <button className="group flex text-white w-fit px-6 py-3 my-2 items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer " >
+                                Portfolio
+                                <span className="group-hover:rotate-90 duration-300" >
+                                    <MdOutlineKeyboardArrowRight size={25} className='ml-1' />
+                                </span>
+                            </button>
+                        </div>
+
+                    </div>
+
+                    <div className="">
+                        <img
+                            src={HeroImage}
+                            alt="hero_image"
+                            className="rounded-2xl mx-auto w-2/3 md:w-full" />
+                    </div>
+
+                </div>
+            </div>
         </>
     )
 }
